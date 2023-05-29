054ca5c7dfb13f6465cc88b3f992d633e59bd653
lorenzoarlo <arlotti.lorenzo2@gmail.com>
2023-05-29 17:27:00 +0200

    added contatori

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let footer = null;
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", async function() {
     footer = document.querySelector("footer");
     const scroll_to_bottom_btn = document.querySelector(".scroll-to-bottom-button");
     const observer = new IntersectionObserver(function(e) {
@@ -7,7 +7,20 @@ window.addEventListener("DOMContentLoaded", function() {
     });
     observer.observe(footer);
 
-})  
+
+    insert_cookie_button();
+    await load_lacb();
+
+    const cookies = lacb.get_cookies();
+    if(cookies['statistics']) {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-78NHLXDQD8');
+    }
+
+});
 
 function scroll_to_bottom() {
     if(footer !== null) footer.scrollIntoView({"behavior": "smooth"});
@@ -18,4 +31,35 @@ function toggle_proof(event) {
     const proof_env = sender.parentElement.parentElement;
     proof_env.querySelector(".environment-body").classList.toggle("hyde");
     sender.innerText = (sender.innerText === 'visibility') ? 'visibility_off' : 'visibility';
+}
+
+function load_lacb() {
+    const LACB_URL = "https://raw.githubusercontent.com/lorenzoarlo/website-utilities/main/lacb.js"
+    return new Promise(function(resolve) {
+        fetch(LACB_URL).then(function(doc) {
+            return doc.text();
+        }).then(function(testo) {
+            const scriptNode = document.createElement("script");
+            scriptNode.textContent = testo;
+            document.body.appendChild(scriptNode);
+
+            resolve(true);
+        }).catch(function(error) {
+            resolve(false);
+        });
+    });   
+}
+
+function insert_cookie_button() {
+    const onAcceptScriptPath  = "https://raw.githubusercontent.com/lorenzoarlo/website-utilities/main/accept.js";
+    const onDeclineScriptPath = "https://raw.githubusercontent.com/lorenzoarlo/website-utilities/main/decline.js";
+    const onFindMorePagePath = "https://lorenzoarlo.github.io/privacy-and-cookies/";
+    
+    
+    const lacb = document.createElement("lorenzoarlo-cookiebutton");
+    lacb.setAttribute("on-accept-script-path", onAcceptScriptPath);
+    lacb.setAttribute("on-decline-script-path", onDeclineScriptPath);
+    lacb.setAttribute("on-find-more-page-path", onFindMorePagePath);
+
+    document.body.appendChild(lacb);
 }
